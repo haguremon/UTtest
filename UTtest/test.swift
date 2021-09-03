3462c087f789ebbe09b4818ce981a7996e3201b2
Yuta iwasaki <yanyong0127@gmail.com>
2021-09-03 16:16:27 +0900

    ユニットテストしてみた　テストケースでもやってみる

---
+++ UTtest/test.swift
@@ -0,0 +1,30 @@
+//
+//  test.swift
+//  UTtest
+//
+//  Created by IwasakIYuta on 2021/09/03.
+// テストするコード
+
+import Foundation
+
+//@testable import UTtest //internalまでなら使える
+ 
+struct Temperature: Equatable {
+    
+    public var celsius: Double
+    
+    public var faherneit: Double {
+        
+        return (9.0 / 5.0) * celsius + 32.0
+    }
+    public init(celsius: Double) {
+        self.celsius = celsius
+    }
+    
+    internal var test: String = "aaa"
+    internal var test1: String = "bbbb"
+
+    private var testprivate: String = "avbc"
+    
+    static let teststatic: String = "static"
+}
--- UTtestTests/UTtestTests.swift
+++ UTtestTests/UTtestTests.swift
@@ -6,28 +6,64 @@
 //
 
 import XCTest
-@testable import UTtest
+@testable import UTtest //テストするディレクトリー staticからinternalまでいける　privateは無理
 
 class UTtestTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    //融点をテストする
+    func testWatherMeltingPoint(){
+        
+        let temperature = Temperature(celsius: 0)
+        //第一、第二がイコールの時に成功
+        XCTAssertEqual(temperature.celsius,0)
+    
+        XCTAssertEqual(temperature.faherneit, 32)
+        XCTAssertEqual(temperature.test, "aaa")
+        XCTAssertEqual(Temperature.teststatic, "static")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    //沸点を調べる
+    
+    func testWatherBoilingPoint(){
+        
+        let temperature = Temperature(celsius: 100)
+        
+        XCTAssertEqual(temperature.celsius,100)
+    
+        XCTAssertEqual(temperature.faherneit, 212)
+    
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    //単一
+    func testSingleExpression() {
+        let value = 5
+        //引数がTrueだったら成功
+        XCTAssert(value == 5)//ここの方が簡潔
+        XCTAssertTrue(value < 9)
+        //引数がfalseだったら成功
+        XCTAssertFalse(value > 100)
+        
+        var nilValue: Int? = nil
+        //nilValue = 1
+        //nilだった場合成功
+        XCTAssertNil(nilValue)
+        nilValue = 1
+        //nilじゃなかったら成功
+        XCTAssertNotNil(nilValue)
+        
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    //二つの式を比較
+    func testTwoExpression(){
+        //それぞれの引数が等しい場合
+        XCTAssertEqual(1, 1)
+       //$1 < $2　の場合成功
+        XCTAssertLessThan(4, 100)
+        //$1 <= $2 の場合成功
+        XCTAssertLessThanOrEqual(4, 4)
+        XCTAssertLessThanOrEqual(4, 10)
+        // $1 > $2 の場合成功
+        XCTAssertGreaterThan("aaa", "a")
+        // $1 >= $2 の場合成功
+        XCTAssertGreaterThanOrEqual("aaa", "a")
+        XCTAssertGreaterThanOrEqual("aaa", "aaa")
     }
-
 }
+
